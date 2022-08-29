2478eda2686d75e93a3c4a31a5ff7feab5b5ce9f
jaac <38704271+jonasaacampos@users.noreply.github.com>
2022-08-29 15:53:40 -0300

    frontEnd
    Modal de inserção de quartos, inserida anotation na view modelo para exibição das labels

--- TheHotelProject2/ViewModel/QuartosViewModel.cs
+++ TheHotelProject2/ViewModel/QuartosViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace TheHotelProject2.ViewModel
 {
@@ -10,12 +11,26 @@ namespace TheHotelProject2.ViewModel
     {
 
         public int QuartoId { get; set; }
+        
+        [Display(Name = "Nº do Quarto")]
         public int QuartoNumero { get; set; }
+
+        [Display(Name = "Foto do Quarto")]
         public string QuartoFoto { get; set; }
+        
+        [Display(Name = "Valor da diária")]
         public decimal QuartoPreco { get; set; }
+
+        [Display(Name = "Disponibilidade para reserva")]
         public int ReservaStatusId { get; set; }
+
+        [Display(Name = "Tipo de Quarto")]
         public int QuartoTipoID { get; set; }
+
+        [Display(Name = "Capacidade (nº de Pessoas)")]
         public int QuartoCapacidade { get; set; }
+
+        [Display(Name = "Descrição")]
         public string QuartoDescricao { get; set; }
 
         public HttpPostedFileBase Image { get; set; }
