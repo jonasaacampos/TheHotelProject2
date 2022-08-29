45d62a44e6ecabe2079a1510355686268e681b95
jaac <38704271+jonasaacampos@users.noreply.github.com>
2022-08-29 14:51:47 -0300

    Controlador Quartos
    Criação de classe e view dos quartos

--- TheHotelProject2/Controllers/QuartosController.cs
+++ TheHotelProject2/Controllers/QuartosController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheHotelProject2.Models;
+using TheHotelProject2.ViewModel;
 
 namespace TheHotelProject2.Controllers
 {
     public class QuartosController : Controller
     {
-        // GET: Quartos
+
+        HotelDBEntities objHotelDBEntities;
+
+        public QuartosController()
+        {
+            objHotelDBEntities = new HotelDBEntities();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            QuartosViewModel objQuartosViewModel = new QuartosViewModel();
+
+            objQuartosViewModel.ListaReservaStatus = (from obj in objHotelDBEntities.ReservaStatus
+                                                      select new SelectListItem()
+                                                      {
+                                                          Text = obj.ReservaStatus1,
+                                                          Value = obj.ReservaStatusId.ToString()
+                                                      }).ToList();
+
+
+            objQuartosViewModel.ListaQuartoTipo = (from obj in objHotelDBEntities.QuartosTipo
+                                                      select new SelectListItem()
+                                                      {
+                                                          Text = obj.QuartoTipo,
+                                                          Value = obj.QuartoTipoID.ToString()
+                                                      }).ToList();
+
+
+            return View(objQuartosViewModel);
         }
     }
 }
---
+++ TheHotelProject2/ViewModel/QuartosViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheHotelProject2.ViewModel
+{
+    public class QuartosViewModel
+    {
+
+        public int QuartoId { get; set; }
+        public int QuartoNumero { get; set; }
+        public string QuartoFoto { get; set; }
+        public decimal QuartoPreco { get; set; }
+        public int ReservaStatusId { get; set; }
+        public int QuartoTipoID { get; set; }
+        public int QuartoCapacidade { get; set; }
+        public string QuartoDescricao { get; set; }
+
+        public HttpPostedFileBase Image { get; set; }
+
+        public List<SelectListItem> ListaReservaStatus { get; set; }
+
+        public List<SelectListItem> ListaQuartoTipo { get; set; }
+
+    }
+}
